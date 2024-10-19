12251e8c8aecac67af49de6c17028602632b241b
Thundervoltgh1 <eniolusi@gmail.com>
2024-10-19 14:57:45 +0100

    Update flappybird.py

--- flappybird.py
+++ flappybird.py
@@ -1,12 +1,20 @@
 import pygame,sys
 pygame.init()
 sw=800
-sh=850
-screen= pygame.display.set_mode((800,850))
+sh=600
+score=0
+flying=False
+screen= pygame.display.set_mode((sw,sh))
 pygame.display.set_caption("FLAPPY BIRD")
 screen.fill("white")
 bg=pygame.image.load('fbg.png')
+ground=pygame.image.load('fbground.png')
+
 game_over=False
+fps=50
+font=pygame.font.SysFont('Bauhaus 93',60)
+
+flying=False
 pygame.display.update()
 
 class Bird(pygame.sprite.Sprite):
@@ -32,11 +40,28 @@ class Bird(pygame.sprite.Sprite):
 flappy=Bird(100,sh/2)
 birdgroup=pygame.sprite.Group()
 birdgroup.add(flappy)
+clock=pygame.time.Clock()
+ground_speed=4
+ground_scroll=0
+def draw_text(txt,x,y):
+    img=font.render(txt,True,'white')
+    screen.blit(img,(x,y))
+
 while True:
+    clock.tick(fps)
     screen.blit(bg,(0,0))
     birdgroup.draw(screen)
+    screen.blit(ground,(ground_scroll,550))
+    draw_text(str(score),10,10)
+    ground_scroll-=ground_speed
+    if abs(ground_scroll)>35:
+        ground_scroll=0
+
+
     birdgroup.update()
     pygame.display.update()
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             sys.exit()
+        if event.type==pygame.MOUSEBUTTONDOWN and flying==False and game_over==False:
+            flying=True
